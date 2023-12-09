38172a2c7983df1c9cf0367a358b5b0e7e46b093
Jalal Jonaid <jalaljonaid@jalals-air.myfiosgateway.com>
2023-12-09 10:58:48 -0500

    Set up initial files

---
+++ backend/src/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+
+const port = process.env.PORT;
+
+app.use(cors());
+
+app.use(express.json())
+
+app.get("*", (req, res) => {
+    res.status(404).json({ success: false, data: { error: "page not found" } });
+});
+
+module.exports = app;
